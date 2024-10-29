ab87642a8ae2421ae9cc69bebbd120117a397ebf
Hirwa Divin <hirwadivin23@gmail.com>
2024-10-29 14:23:28 -0400

    Installed and imported figlet package, updated script to type 'Hello, World\!'

--- quickstart/index.ts
+++ quickstart/index.ts
@@ -1,7 +1,10 @@
+import figlet from "figlet";
+
 const server = Bun.serve({
   port: 3000,
   fetch(req) {
-    return new Response("Bun!");
+    const body = figlet.textSync("Hello, World!");
+    return new Response(body);
   },
 });
 
