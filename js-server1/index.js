d127496dd8172c208f6cb6523b90329fffe6b5c7
hirwadivin1 <hirwadivin23@gmail.com>
2024-10-30 15:07:28 -0400

    update to include /get/ and /post/

--- js-server1/index.js
+++ js-server1/index.js
@@ -3,8 +3,33 @@ import express from "express";
 const app = express();
 const port = 5000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-	res.send("Hello from Express on Bun!");
+	res.send("Hello, World!");
+});
+
+app.get("/get/", (req, res) => {
+	let text = {
+		player_name: "Divin",
+		games: ["Elden Ring","Red Dead II","God Of War","FC 24"],
+		play_time: "+100 hrs",
+		review: "Suffering",
+		comment: "Could've been coding"
+	};
+	res.json(text);
+});
+
+app.post("/post/", (req, res) => {
+	let num = req.body.num;
+	if (typeof num === "undefined") {
+		res.status(400).json({error: "'num' parameter is undefined"});
+	} else if (typeof num !== "number"){
+		res.status(400).json({error: "'num' must be a number"});
+	} else {
+		const isEven = num % 2 === 0;
+		res.json({number: num, isEven: isEven});
+	};
 });
 
 app.listen(port, () => {
