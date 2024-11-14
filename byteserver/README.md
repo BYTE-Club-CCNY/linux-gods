d278e03766722b931551be43ae32e3576a545e89
Feras Chahid <feraschahid@gmail.com>
2024-11-14 12:58:25 -0500

    Pull request to main (#5)
    * Trial :)
    
    * Removing trial.txt
    
    * Creating bun quickstart
    
    * Modified index.ts to use Bun API
    
    * added a script(bun run bunny)
    
    * Fixed & verified that script can run bunny
    
    * Installed and imported figlet package, updated script to type 'Hello, World\!'
    
    * Managed to make a simple server that returns 'Hello, World\!'
    
    * Deleted the sample server as I was using .ts
    
    * Created an Express server with Bun as the runtime, found out that res.send is not that compatible with Bun
    
    * update to include /get/ and /post/
    
    * Modified README; Added figlet to index.js
    
    * Minor correction to README
    
    * Made a Dockerfile, with help from Bun's official server
    
    * Removed ENV command, as it was unnecessary
    
    * Java Syntax
    
    * Tinkered with the express server
    
    * The Micronaut server with simple endpoints
    
    * create two get endpoints
    
    * add API endpoints to README.md
    
    * update README.md with information about how to run the server
    
    * update README.md formating and info
    
    * Deleted simple-server
    
    * Copied my micronaut app into the repo
    
    * Revert "Trying PR"
    
    * Added figlet to server
    
    * Renamed server to get conflicts to resolve
    
    * Cleaned branch to prepare for merge
    
    * merge java endpoints
    
    * Finalizing merge
    
    * Updated .gitignore
    
    * Cleaning merged branch
    
    ---------
    
    Co-authored-by: hirwadivin1 <hirwadivin23@gmail.com>
    Co-authored-by: Ayesha Ilyas <annie16950@gmail.com>
    Co-authored-by: Hirwa Divin Iradukunda <145731804+HirwaDivin1@users.noreply.github.com>

--- byteserver/README.md
+++ byteserver/README.md
@@ -1,3 +1,28 @@
+# Micronaut HTTP Server Example
+
+## API Spec
+**Base url** (for local testing): ``http://localhost:8080``
+
+``GET /``
+- Returns the text "api is operational"
+
+``GET /get/``
+- Query parameters are ``name``, ``cohort``, and ``team``. All of these are optional. If one or more are not provided, the value is considered an empty string.
+- Echos team information in a json list
+- Example request: ``http://localhost:8080/get?name=linux+gods&cohort=two&team=4``
+- Response: ``["linux gods", "two", "4"]``
+
+## How to run the server
+1. Install GraalVM: ``sdk install java 17.0.12-graal`` You need ``sdk`` installed to use to run the above command. See https://sdkman.io/install/ 
+2. Switch to using GraalVM as the Java Runtime: ``sdk use java 17.0.12-graal``
+3. Build the server: ``./mvnw package -Dpackaging=native-image`` This may take a while.
+4. Run the executable. From the main project directory (linux-gods), run ``byteserver/target/byteserver``
+
+Now use curl or a browser to access the two endpoints.
+
+For more info, see the offical micronaut getting started guide: https://guides.micronaut.io/latest/creating-your-first-micronaut-app-maven-java.html
+
+
 ## Micronaut 4.6.3 Documentation
 
 - [User Guide](https://docs.micronaut.io/4.6.3/guide/index.html)
---
+++ byteserver/src/main/java/ccnybyte/server/TeamController.java
@@ -0,0 +1,53 @@
+package ccnybyte.server;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.QueryValue;
+
+import java.util.*;
+
+@Controller("/")
+
+public class TeamController{
+    private List<String[]> dataStore = new ArrayList<>();
+
+    // TODO: 1) return 'API is operational'
+
+    @Get 
+    @Produces(MediaType.TEXT_PLAIN)
+    public String home() {
+        String message = "API is operational";
+        
+        return message;
+    }
+    
+    // TODO: 2) Get and store 3 parameters
+    
+    @Get("get")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getTeam(
+            @QueryValue Optional<String> name,
+            @QueryValue Optional<String> cohort,
+            @QueryValue Optional<String> team
+            ) {
+        System.out.println("Query parameter values: ");
+        System.out.println("Name: " + name);
+        System.out.println("Cohort: " + cohort);
+        System.out.println("Team: " + team);
+
+        System.out.println("Storing parameters in array");
+        String[] teamInfo = { name.orElse(""), cohort.orElse(""), team.orElse("") };
+
+        dataStore.add(teamInfo);
+
+        return "Data added: Name = " + name + ", Cohort = " + cohort + ", Team = " + team;
+    }
+
+    @Get("all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<String[]> getAllData() {
+        return dataStore;
+    }
+}
